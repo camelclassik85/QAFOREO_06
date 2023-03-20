a9799b24a085fda8452fe283e852d5645439e806
Alexander_Dulalaev <camel1985@mail.ru>
2023-03-20 10:43:38 +0300

    upd hw3

--- hw3.py
+++ hw3.py
@@ -47,6 +47,8 @@
 # print(*my_dict.values())
 #for kv in my_dict.items():
 #    print(kv)
+#for k, v in my_dict.items():
+#    print(f'{k}: {v})
 
 #
 # 3.6. Найдите сумму всех значений в словаре my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}
